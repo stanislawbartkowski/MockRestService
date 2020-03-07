4dc90e193368c4896ee5e3ed8c1673447f9be9d3
sbartkowski <stanislawbartkowski@gmail.com>
2020-03-07 01:54:25 +0100

    HTTPS server

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,8 @@
 PORT=9800
-java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT
+# secure connection, uncomment below
+KEYSTOREFILE=/home/sbartkowski/x/mykey.keystore
+KEYPASSWORD=secret
+STOREPASSWORD=secret
+ALIAS=alias
+# ---------
+java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $KEYSTOREFILE $KEYPASSWORD $STOREPASSWORD $ALIAS
--- src/main/java/com/org/mockrestservice/MockRestService.java
+++ src/main/java/com/org/mockrestservice/MockRestService.java
@@ -2,7 +2,8 @@ package com.org.mockrestservice;
 
 import com.rest.restservice.*;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 
 public class MockRestService extends RestStart {
@@ -11,18 +12,53 @@ public class MockRestService extends RestStart {
         System.out.println(s);
     }
 
+    private static final String STOREKEY = "store.key.filename";
+    private static final String STOREPASSWORD = "key.store.password";
+    private static final String ALIAS = "alias";
+
     private static void help() {
-        P("Parameters: /port number");
-        P("Example");
-        P(" java ...  9800 ");
+        P(" Non-secure HTTP connection:");
+        P("   Parameters: /port number/");
+        P("     Example");
+        P("       java ...  9800 ");
+        P("");
+        P(" Secure HTTP connection:");
+        P("      Parameters: /port number/ /secure conf/");
         System.exit(4);
     }
 
+    private static String getParam(Properties prop, String key) throws IOException {
+        String res = prop.getProperty(key);
+        if (res == null || "".equals(res)) {
+            String mess = "Parameter " + key + " not found in the secure property file";
+            RestLogger.L.severe(mess);
+            throw new IOException(mess);
+        }
+        return res;
+    }
+
+    private static String[] readConf(String filename) throws IOException {
+        try (InputStream input = new FileInputStream(filename)) {
+            Properties prop = new Properties();
+            prop.load(input);
+            return new String[]{
+                    getParam(prop, STOREKEY),
+                    getParam(prop, STOREPASSWORD),
+                    getParam(prop, STOREPASSWORD),
+                    getParam(prop, ALIAS)
+            };
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        if (args.length != 1) {
+        if (args.length != 1 && args.length != 2) {
             help();
         }
         int PORT = Integer.parseInt(args[0]);
-        RestStart(PORT, (server) -> new RestServices().registerServices(server));
+        RestStart(PORT, (server) -> new RestServices().registerServices(server),
+                (args.length == 1) ?
+                        new String[]{} :
+                        readConf(args[1])
+        );
     }
 }
