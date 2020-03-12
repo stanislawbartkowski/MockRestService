515b319633553001dd07d89c510f9a5063e2305e
sbartkowski <stanislawbartkowski@gmail.com>
2020-03-12 13:48:21 +0000

    script template changes

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,4 @@
 PORT=9800
-# secure connection, uncomment below
-KEYSTOREFILE=/home/sbartkowski/x/mykey.keystore
-KEYPASSWORD=secret
-STOREPASSWORD=secret
-ALIAS=alias
-# ---------
-java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $KEYSTOREFILE $KEYPASSWORD $STOREPASSWORD $ALIAS
+SECURITY=secure.properties
+#SECURITY=
+java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $SECURITY
--- template/runserver.sh
+++ template/runserver.sh
@@ -1,2 +1,4 @@
 PORT=9800
-java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT
+SECURITY=secure.properties
+#SECURITY=
+java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $SECURITY
