9439b8fb2ae97ff47c6819ba7fe7362849778bc6
sb <sb@thinkde.sb.com>
2020-05-07 12:55:21 +0200

    modify template including Kerberos

--- template/runserver.sh
+++ template/runserver.sh
@@ -1,4 +1,6 @@
 PORT=9800
-SECURITY=secure.properties
+#SECURITY=secure.properties
 #SECURITY=
-java -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $SECURITY
+KERBEROS="-Djava.security.auth.login.config=$PWD/server_jaas.conf"
+#KERBEROSDEBUG=-Dsun.security.krb5.debug=true
+java $KERBEROS $KERBEROSDEBUG -cp target/MockRestService-1.0-SNAPSHOT-jar-with-dependencies.jar com.org.mockrestservice.MockRestService $PORT $SECURITY
